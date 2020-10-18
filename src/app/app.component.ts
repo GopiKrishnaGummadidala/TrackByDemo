110776128efd4040d4bc57392cf48990c9d25aef
Gopi <gopikrishna.gummadidala@gmail.com>
2020-10-18 18:22:52 +0200

    code changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+export interface Item {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'trackByDemo';
+export class AppComponent implements OnInit {
+  items: Item[] = [];
+
+  ngOnInit(): void {
+    this.items.push({id: 1, name: 'Lappy'});
+    this.items.push({id: 2, name: 'Phone'});
+    this.items.push({id: 3, name: 'Ear phones'});
+  }
+
+  refreshItems(): void {
+    this.items = [{id: 1, name: 'Lappy'},{id: 2, name: 'Phone'},{id: 3, name: 'Ear phones'}, {id: 4, name: 'Headsets'}];
+  }
+
+  trackByMethod(item: Item): number {
+    return item.id;
+  }
 }
